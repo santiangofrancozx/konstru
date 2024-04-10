fb0b2e2ec7c94ee0edf1b3669cdee786d4d1e39b
Santiago Franco De la rosa <santiagofrancodelarosa@Santiagos-Laptop.local>
2024-04-10 11:19:49 -0500

    [INITIALS] Modificaciones a el frontend de budget, actualizacion de los CSV de migracion estandares, cambios en el manejador de busquedas para filtrar solo actividades

--- backend/main.go
+++ backend/main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	//DSN := "" // Define aqui tu DSN para desarrollo en local
 	// strutura de DSN user:password@tcp(127.0.0.1:3306)/?charset=utf8mb4&parseTime=True&loc=Local
-	config.SetDSN("")
+	config.SetDSN("root:safraroot@tcp(127.0.0.1:3306)/?charset=utf8mb4&parseTime=True&loc=Local")
 	//cambia user por tu usuario de MySql y password por tu contrasenia de acceso a el puerto, por defecto uso 3306
 	//handlers.ExecuteMigrations(config.DB_DSN) // ejecut las migraciones crea la db si no existe y las tablas en esta si no existen
 	//handlers.ImportDataFromCSVDB(config.DB_DSN)
