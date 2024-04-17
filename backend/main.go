08d852cedde62a9ddfdfa6dbda40ad3759817d4e
Santiago Franco De la rosa <santiagofrancodelarosa@Santiagos-Laptop.local>
2024-04-17 11:31:22 -0500

    Main DSN ignore

--- backend/main.go
+++ backend/main.go
@@ -10,10 +10,10 @@ import (
 func main() {
 	//DSN := "" // Define aqui tu DSN para desarrollo en local
 	// strutura de DSN user:password@tcp(127.0.0.1:3306)/?charset=utf8mb4&parseTime=True&loc=Local
-	config.SetDSN("")
+	config.SetDSN("root:safraroot@tcp(127.0.0.1:3306)/?charset=utf8mb4&parseTime=True&loc=Local")
 	//cambia user por tu usuario de MySql y password por tu contrasenia de acceso a el puerto, por defecto uso 3306
-	handlers.MakeMigrations(config.DB_DSN, handlers.ExecuteMigrations()) // ejecut las migraciones crea la db si no existe y las tablas en esta si no existe
-	//handlers.ImportDataFromCSVDB(config.DB_DSN)
+	//handlers.MakeMigrations(config.DB_DSN, handlers.ExecuteMigrations()) // ejecut las migraciones crea la db si no existe y las tablas en esta si no existe
+	handlers.ImportDataFromCSVDB(config.DB_DSN)
 
 	router := gin.Default()
 	gin.SetMode(gin.DebugMode) // o gin.ReleaseMode
